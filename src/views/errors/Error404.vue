5dd9506cbbe2c4ddb903c9b9a33cea375e1311c3
Kamil <80323556+Kamil6789@users.noreply.github.com>
2021-10-20 11:29:26 +0200

    Modified 404 page

--- src/views/errors/Error404.vue
+++ src/views/errors/Error404.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>404</h1>
+    <main>
+        <div class="container">
+            <div class="header">
+                <div class="header-content">
+                    <hgroup>
+                        <h1 class="err display-1 text-white">404</h1>
+                        <h2 class="err h4 text-white">Nie odnaleziono strony.</h2>
+                    </hgroup>
+                </div>
+            </div>
+        </div>
+        <particles-bg class="z-index-1" type="thick" :canvas="{backgroundColor:'#888'}" :bg="true" />
+    </main>
 </template>
+
+<script>
+import {ParticlesBg} from "particles-bg-vue";
+
+export default {
+    name: "404",
+    components: {
+        ParticlesBg
+    }
+}
+</script>
