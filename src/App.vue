55202a8f69500cca5b6159e609a06dcf9eee0542
Kamil <80323556+Kamil6789@users.noreply.github.com>
2021-10-22 11:22:43 +0200

    Added handling for error 500

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="app">
         <main role="main">
-            <router-view />
+            <Error500 v-if="err500" />
+            <router-view v-else />
         </main>
     </div>
 </template>
@@ -16,14 +17,22 @@ import "bootstrap/dist/js/bootstrap.js"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 
+import Error500 from "./views/errors/Error500.vue"
+
 export default {
     name: "App",
-    components: {},
+    components: {Error500},
     async created() {
         const res = await fetch("/api/user/info");
+        if(res.status == 500) return this.err500 = true;
         const body = await res.json();
         if(body.success && window.location.pathname == "/") this.$router.push("/panel");
         else if(window.location.pathname.startsWith("/panel") && !body.success) this.$router.push("/");
+    },
+    data() {
+        return {
+            err500: false
+        }
     }
 }
 </script>
---
+++ src/views/errors/Error500.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="header">
+                <div class="header-content">
+                    <hgroup>
+                        <h1 class="err display-1 text-white">500</h1>
+                        <h2 class="err h4 text-white">Wewnętrzny błąd serwera.</h2>
+                    </hgroup>
+                </div>
+            </div>
+        </div>
+        <particles-bg class="z-index-1" type="thick" :canvas="{backgroundColor:'#888'}" :bg="true" />
+    </main>
+</template>
+
+<script>
+import {ParticlesBg} from "particles-bg-vue";
+
+export default {
+    name: "500",
+    components: {
+        ParticlesBg
+    }
+}
+</script>
