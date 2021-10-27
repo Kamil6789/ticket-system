265cea30dfc6f45429441fa93470331ca2cd3f13
Kamil <80323556+Kamil6789@users.noreply.github.com>
2021-10-27 11:46:23 +0200

    Preventing sending comments when ticket is closed

--- api/routes/tickets.js
+++ api/routes/tickets.js
@@ -5,6 +5,7 @@ module.exports = function (app) {
     app.post('/api/tickets/comment', checkAuth, async (req, res, next) => {
         if (req.user && req.body.content && req.body.ticketId) {
             database.getTicketById(req.body.ticketId).then(async ticket => {
+                if (ticket.status == 2) return;
                 if (ticket.userId == req.user.id || ticket.technicianId == req.user.id) {
                     await database.postComment(ticket, req.user, req.body.content);
                     if (req.user.id == ticket.technicianId) {
